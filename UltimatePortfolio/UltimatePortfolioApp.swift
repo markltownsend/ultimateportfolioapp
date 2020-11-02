09e0d7cbf9320686c33d443bb2ad3f5b19ed07f5
Mark Townsend <markltownsend@users.noreply.github.com>
2020-11-02 05:52:01 -0800

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Mark Townsend on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
