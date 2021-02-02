4643dd2d395e0bd69f4aa029d585436b9f48e2ee
Mark Townsend <markltownsend@users.noreply.github.com>
2021-02-02 06:34:50 -0800

    HomeView implementation
    Beginning of localization

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Mark Townsend on 1/30/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
