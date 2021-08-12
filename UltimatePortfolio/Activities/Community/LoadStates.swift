7af967adc31018380cc8efd216fdfd0b3386f5bb
Mark Townsend <markltownsend@users.noreply.github.com>
2021-08-12 10:31:07 -0700

    Added Community tab for displaying shared projects and items from iCloud

---
+++ UltimatePortfolio/Activities/Community/LoadStates.swift
@@ -0,0 +1,12 @@
+//
+//  LoadStates.swift
+//  LoadStates
+//
+//  Created by Mark Townsend on 8/12/21.
+//
+
+import Foundation
+
+enum LoadState {
+    case inactive, loading, success, noResults
+}
---
+++ UltimatePortfolio/Activities/Community/SharedItem.swift
@@ -0,0 +1,15 @@
+//
+//  SharedItem.swift
+//  SharedItem
+//
+//  Created by Mark Townsend on 8/12/21.
+//
+
+import Foundation
+
+struct SharedItem: Identifiable {
+    let id: String
+    let title: String
+    let detail: String
+    let completed: Bool
+}
